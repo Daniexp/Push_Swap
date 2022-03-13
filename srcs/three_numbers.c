c3023f222a10cf6b9e23a35e65ed6e55c622cbba
Daniel Exposito lopez <dexposit@c2r11s6.42madrid.com>
2022-03-13 12:35:15 +0100

    better 3numb

--- srcs/three_numbers.c
+++ srcs/three_numbers.c
@@ -6,7 +6,7 @@
 /*   By: dexposit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 20:49:51 by dexposit          #+#    #+#             */
-/*   Updated: 2022/03/12 21:32:28 by dexposit         ###   ########.fr       */
+/*   Updated: 2022/03/13 12:33:27 by dexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,17 @@ void	three_num(t_list **stc)
 		if (get_top(stc[0]) - get_cnt(stc[0], 2) == 1)
 			put_mv("sa", stc);
 		else
-		{
-			if (get_top(stc[0]) - get_bottom(stc[0]) == 1)
-			   put_mv("rra", stc);
+		{	if (get_bottom(stc[0]) - get_cnt(stc[0],2) == 1)
+				put_mv("ra", stc);
 			else
 			{
-				put_mv("sa", stc);
-				put_mv("ra", stc);
+				if (get_top(stc[0]) - get_bottom(stc[0]) == 1)
+					put_mv("rra", stc);
+				else
+				{
+					put_mv("sa", stc);
+					put_mv("ra", stc);
+				}
 			}
 		}
 	}	
