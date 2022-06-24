8fb33c8a10a280c044dc26e88864b1346fcbb38e
Daniel Exposito lopez <dexposit@c2r4s1.42madrid.com>
2022-06-24 11:54:41 +0200

    delete comment

--- main.c
+++ main.c
@@ -6,23 +6,11 @@
 /*   By: dexposit <dexposit@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 15:29:53 by dexposit          #+#    #+#             */
-/*   Updated: 2022/03/26 22:01:15 by dexposit         ###   ########.fr       */
+/*   Updated: 2022/06/24 11:54:19 by dexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./incl/push_swap.h"
-
-/*
- *	The push_swap must to take several numbers in the sanme argument and split
- *	then with a space ex "2 3 4" this are 2->3->4
- *	repeat this process for all the arguments.
- *	the split give us a **str with the strings of the number for position so
- *	we have to convert it with the atoi to and int.
- *	if we can´t convert some split result in to a int we stop the program and
- *	exit and error.
- *		stop the program -> free the split and the list
- *	if the convert was success we put it in a t_list
- */
 
 int	main(int argc, char **argv)
 {
