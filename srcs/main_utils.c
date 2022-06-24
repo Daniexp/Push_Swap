7c4e5d412af41d0b092c71c424824dbde2ff9f7e
Daniel Exposito lopez <dexposit@c2r4s1.42madrid.com>
2022-06-24 14:12:46 +0200

    add inf in print_stc function

--- srcs/main_utils.c
+++ srcs/main_utils.c
@@ -6,7 +6,7 @@
 /*   By: dexposit <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 18:51:06 by dexposit          #+#    #+#             */
-/*   Updated: 2022/03/26 22:02:12 by dexposit         ###   ########.fr       */
+/*   Updated: 2022/06/24 14:09:40 by dexposit         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,13 @@ void	print_stc(t_list *stc)
 {
 	t_list	*aux;
 
+	printf("                   TOP\n");
 	aux = stc;
 	while (aux)
 	{
+		printf("                    ");
 		printf("%d\n", *(int *)aux->content);
 		aux = aux->next;
 	}
+	printf("                  BOTTOM\n");
 }
